4bdb4634c2f9701833c3bc93ec2b97ca817231a2
Peter Kimani <peterjajakim@gmail.com>
2022-06-09 14:06:01 +0300

    NAVBAR, .JS FUNCTIONALITY, IMAGE

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 window.alert("HAPPY CODING");
+
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
